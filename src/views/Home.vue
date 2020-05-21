f25f0044eb3108a2cbfda6c79fa0c9bc8616d0ba
Singgih <gokonggokong71@gmail.com>
2020-05-21 09:43:14 +0700

    converting html to vue home page

---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <Header></Header>
+    <Hero></Hero>
+    <WomenBanner></WomenBanner>
+    <Instagram></Instagram>
+    <Partner></Partner>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+import Header from "@/components/Header.vue";
+import Hero from "@/components/Hero.vue";
+import WomenBanner from "@/components/WomenBanner.vue";
+import Instagram from "@/components/Instagram.vue";
+import Partner from "@/components/Partner.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "home",
+  components: {
+    Header,
+    Hero,
+    WomenBanner,
+    Instagram,
+    Partner,
+    Footer
+  }
+};
+</script>
